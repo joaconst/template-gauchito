8383fb3423a7d7aad5122b8c3fba2af6562906e1
joacnt <constabeljoaquinn@gmail.com>
2025-04-07 19:56:00 -0300

    Modifica el componente Header: se añade un fondo al encabezado para mejorar la visibilidad y el diseño.

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -8,7 +8,7 @@ const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className=" sticky top-0 z-50">
+    <header className="sticky top-0 bg-background z-50">
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
